dc67f029cd750b184f787ab2494598cc1c6e9388
sparshshekhar <115075888+sparshshekhar@users.noreply.github.com>
2024-11-12 23:56:37 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slides img");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove("active");
+        if (i === index) {
+            slide.classList.add("active");
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
